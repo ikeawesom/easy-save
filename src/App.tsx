8bcd9a7f24fa546f58a856f182791766f8fdb493
ikeawesom <ikelimjl@gmail.com>
2025-11-17 00:26:25 +0800

    added page feature

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ import { Modal } from "./components/Modal";
 import TextInput from "./components/TextInput";
 import { useSavings } from "./hooks/useSavings";
 import { formatMoney } from "./utils";
+import { pages, type PageType } from "./lib";
+import { usePage } from "./hooks/usePage";
 
 export default function App() {
   const {
-    savings,
-    monthlySavings,
+    savingGoal,
+    monthlySavingGoal,
     locked,
     error,
     isModalOpen,
@@ -21,8 +23,10 @@ export default function App() {
     isLoading,
   } = useSavings();
 
+  const { page, setPage } = usePage();
+
   const isInvalidSavings =
-    savings <= 0 || monthlySavings <= 0 || monthlySavings > savings;
+    savingGoal <= 0 || monthlySavingGoal <= 0 || monthlySavingGoal > savingGoal;
 
   if (isLoading) {
     return (
@@ -33,56 +37,74 @@ export default function App() {
   }
 
   return (
-    <section className="w-full flex items-center justify-start flex-col gap-6 p-10 min-h-screen bg-linear-to-br from-cyan-100 via-cyan-50 to-white">
-      <h1 className="w-full text-center text-4xl">
+    <section className="w-full flex items-center justify-start flex-col gap-2 p-10 min-h-screen bg-linear-to-br from-cyan-100 via-cyan-50 to-white">
+      <h1 className="w-full text-center text-4xl mb-4">
         Easy<span className="font-bold text-cyan-600">Save</span>
       </h1>
 
-      <Container>
-        {locked ? (
-          <div className="flex flex-col gap-6">
-            <div className="flex items-center justify-between gap-2">
-              <div>
-                <p className="text-gray-700">Savings goal:</p>
-                <span className="text-3xl font-bold text-cyan-600">
-                  ${formatMoney(savings ?? 0)}
-                </span>
+      {locked && (
+        <Container className="flex items-center justify-evenly p-0 overflow-hidden">
+          {pages.map((item: PageType) => (
+            <button
+              onClick={() => setPage(item.id)}
+              key={item.id}
+              className="flex-1 hover:bg-black/10 py-4"
+            >
+              <p className={page === item.id ? "text-cyan-600 font-bold" : ""}>
+                {item.name}
+              </p>
+            </button>
+          ))}
+        </Container>
+      )}
+
+      {page === "settings" && (
+        <Container>
+          {locked ? (
+            <div className="flex flex-col gap-6">
+              <div className="flex items-center justify-between gap-2">
+                <div>
+                  <p className="text-gray-700">Savings goal:</p>
+                  <span className="text-3xl font-bold text-cyan-600">
+                    ${formatMoney(savingGoal ?? 0)}
+                  </span>
+                </div>
+
+                <button
+                  className={twMerge(
+                    "bg-white text-gray-800 rounded-md py-2 px-3 border shadow-sm border-gray-100 hover:bg-gray-50 transition-colors"
+                  )}
+                  onClick={openModal}
+                  type="button"
+                >
+                  ✎
+                </button>
               </div>
 
+              <div className="flex items-center justify-between gap-2">
+                <div>
+                  <p className="text-gray-700">Monthly goal:</p>
+                  <span className="text-3xl font-bold text-cyan-600">
+                    ${formatMoney(monthlySavingGoal ?? 0)}
+                  </span>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <div className="flex flex-col gap-4 items-center justify-center py-8">
+              <h3 className="text-gray-600 text-center">
+                Set your savings goals to get started!
+              </h3>
               <button
-                className={twMerge(
-                  "bg-white text-gray-800 rounded-md py-2 px-3 border shadow-sm border-gray-100 hover:bg-gray-50 transition-colors"
-                )}
+                className="bg-cyan-600 text-white text-lg shadow-md rounded-md py-2 px-6 hover:bg-cyan-700 transition-colors"
                 onClick={openModal}
-                type="button"
               >
-                ✎
+                Begin Saving
               </button>
             </div>
-
-            <div className="flex items-center justify-between gap-2">
-              <div>
-                <p className="text-gray-700">Monthly goal:</p>
-                <span className="text-3xl font-bold text-cyan-600">
-                  ${formatMoney(monthlySavings ?? 0)}
-                </span>
-              </div>
-            </div>
-          </div>
-        ) : (
-          <div className="flex flex-col gap-4 items-center justify-center py-8">
-            <h3 className="text-gray-600 text-center">
-              Set your savings goals to get started!
-            </h3>
-            <button
-              className="bg-cyan-600 text-white text-lg shadow-md rounded-md py-2 px-6 hover:bg-cyan-700 transition-colors"
-              onClick={openModal}
-            >
-              Begin Saving
-            </button>
-          </div>
-        )}
-      </Container>
+          )}
+        </Container>
+      )}
 
       <Modal isOpen={isModalOpen} onClose={closeModal} allowClose={false}>
         <div className="flex flex-col gap-4">
@@ -93,7 +115,7 @@ export default function App() {
           <TextInput
             enabled={true}
             onChange={onChangeSavings}
-            value={savings}
+            value={savingGoal}
             id="save_amt"
             label="Enter savings goal:"
           />
@@ -101,7 +123,7 @@ export default function App() {
           <TextInput
             enabled={true}
             onChange={onChangeMonthlySavings}
-            value={monthlySavings}
+            value={monthlySavingGoal}
             id="monthly_save_amt"
             label="Enter monthly savings:"
           />
---
+++ src/hooks/usePage.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+import { useStorageState } from "./useStorageState";
+
+export function usePage() {
+    const [page, setPage] = useState("home");
+    const [storedSavings] = useStorageState("saving_goal");
+
+    useEffect(() => {
+        console.log("Stored savings:", storedSavings);
+        if (storedSavings) {
+            setPage("home");
+        } else {
+            setPage("settings");
+        }
+    }, [storedSavings]);
+
+    return { page, setPage };
+}

d3da3c56f86399ed090d2f9befff56fa8b84d559
ikeawesom <ikelimjl@gmail.com>
2025-11-17 00:27:04 +0800

    added global type and constant

---
+++ src/lib.ts
@@ -0,0 +1,10 @@
+export const pages = [
+    { id: "home", name: "Home" },
+    { id: "expenses", name: "Expenses" },
+    { id: "settings", name: "Settings" }
+] as PageType[];
+
+export interface PageType {
+    id: string;
+    name: string;
+}

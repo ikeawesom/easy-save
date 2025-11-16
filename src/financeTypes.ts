c66dab900ca26434fc33f9798770ab4d3ff688a7
ikeawesom <ikelimjl@gmail.com>
2025-11-17 01:40:38 +0800

    added finance types

---
+++ src/financeTypes.ts
@@ -0,0 +1,14 @@
+export interface ExpensesType {
+    id: string;
+    date: Date;
+    amount: number;
+    category: string;
+    description?: string;
+}
+
+export interface SavingsType {
+    id: string;
+    date: string;
+    amount: number;
+}
+

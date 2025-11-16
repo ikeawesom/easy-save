cd123189668e6f0448547961a86dd982d54d7b2d
ikeawesom <ikelimjl@gmail.com>
2025-11-17 01:39:33 +0800

    created hook to add savings

---
+++ src/hooks/useAddSavings.ts
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import { useStorageState } from "./useStorageState";
+import type { SavingsType } from "../financeTypes";
+
+export function useAddSavings() {
+    const [currentSavings, setCurrentSavings] = useStorageState("current_savings");
+    const [savings, setSavings] = useState(0);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isSubmit, setIsSubmit] = useState(false);
+
+    useEffect(() => {
+        if (savings > 0 && !isModalOpen && isSubmit) {
+            // submitted
+            const updatedSavings = currentSavings ? [...currentSavings, { amount: savings, date: new Date().toISOString() }] : [{ amount: savings, date: new Date().toISOString() }] as SavingsType[];
+            setCurrentSavings(updatedSavings);
+            reset();
+        }
+    }, [savings, isModalOpen])
+
+    const onChangeSavings = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = parseFloat(e.target.value);
+        if (!isNaN(value)) {
+            setSavings(value);
+        } else {
+            setSavings(0);
+        }
+    };
+
+    const reset = () => {
+        setIsModalOpen(false); setIsSubmit(false); setSavings(0);
+    }
+    const openModal = () => setIsModalOpen(true);
+
+    const closeModal = () => { reset() };
+
+    const submit = () => {
+        if (savings > 0) { setIsModalOpen(false); setIsSubmit(true); }
+    };
+
+    return {
+        openModal, closeModal, onChangeSavings, submit, isModalOpen, savings, currentSavings
+    }
+}

abd0f4c211072d4c399a343304f888dde3db7577
ikeawesom <ikelimjl@gmail.com>
2025-11-16 21:42:22 +0800

    simplified hooks

--- src/hooks/useSavings.tsx
+++ src/hooks/useSavings.tsx
@@ -1,62 +1,67 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
+import { useLocalStorageState } from "./useLocalStorageState";
 
 export function useSavings() {
-  const [savings, setSavings] = useState<number>();
+  const [savings, setSavings] = useLocalStorageState("savings_goal");
+  const [monthlySavings, setMonthlySavings] =
+    useLocalStorageState("monthly_savings");
+
   const [locked, setLocked] = useState(false);
-  const [saveSavings, setSaveSavings] = useState(false);
   const [error, setError] = useState<string>();
 
   useEffect(() => {
-    const cur = localStorage.getItem("savings_goal");
-
-    if (cur) {
-      setSavings(parseFloat(cur));
-      setSaveSavings(true);
+    if (savings > 0 && monthlySavings > 0) {
       setLocked(true);
-    } else {
-      setLocked(false);
-      setSavings(0);
     }
   }, []);
-
-  useEffect(() => {
-    if (savings !== undefined && saveSavings) {
-      localStorage.setItem("savings_goal", savings.toString());
-    }
-  }, [saveSavings]);
 
   const onChangeSavings = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = parseFloat(e.target.value);
     if (!isNaN(value)) {
       setSavings(value);
-      setSaveSavings(false);
     } else {
       setSavings(0);
     }
   };
 
-  const submitSavings = (e: React.FormEvent) => {
+  const onChangeMonthlySavings = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = parseFloat(e.target.value);
+    if (!isNaN(value)) {
+      setMonthlySavings(value);
+    } else {
+      setMonthlySavings(0);
+    }
+  };
+
+  const submit = (e: React.FormEvent) => {
     e.preventDefault();
+
     if (locked) {
       setLocked(false);
+      return;
+    }
+
+    if (savings && monthlySavings && savings > 0 && monthlySavings > 0) {
+      setError(undefined);
+      setLocked(true);
     } else {
-      if (savings !== undefined && savings > 0) {
-        setError(undefined);
-        setLocked(true);
-        setSaveSavings(true);
-      } else {
-        setError("Please enter a valid savings amount greater than 0!");
-      }
+      setError("Please enter valid savings values greater than 0!");
     }
   };
 
   return {
+    // values
     savings,
+    monthlySavings,
+
+    // state
     locked,
-    onChangeSavings,
-    saveSavings,
-    submitSavings,
-    setLocked,
     error,
+
+    // handlers
+    onChangeSavings,
+    onChangeMonthlySavings,
+    submit,
+    setLocked,
   };
 }

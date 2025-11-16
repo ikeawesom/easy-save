050ecc93a5cd227f9c9233571c532ff962a46a91
ikeawesom <ikelimjl@gmail.com>
2025-11-16 22:29:09 +0800

    modified name

---
+++ src/hooks/useStorageState.ts
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+
+export function useStorageState(key: string) {
+    const [value, setValue] = useState<number>(0);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const loadValue = async () => {
+            try {
+                const result = localStorage.getItem(key);
+                if (result) {
+                    setValue(JSON.parse(result));
+                }
+            } catch (error) {
+                console.log('Key not found or error loading:', error);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+        loadValue();
+    }, [key]);
+
+    useEffect(() => {
+        if (!isLoading) {
+            const saveValue = async () => {
+                try {
+                    localStorage.setItem(key, JSON.stringify(value));
+                } catch (error) {
+                    console.error('Error saving:', error);
+                }
+            };
+            saveValue();
+        }
+    }, [key, value, isLoading]);
+
+    return [value, setValue, isLoading] as const;
+}

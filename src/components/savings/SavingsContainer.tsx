e1adbf13e2bb408257713f459ebab7c5149bc328
ikeawesom <ikelimjl@gmail.com>
2025-11-21 02:16:40 +0800

    added monthly savings prop

--- src/components/savings/SavingsContainer.tsx
+++ src/components/savings/SavingsContainer.tsx
@@ -1,34 +1,32 @@
 import { twMerge } from "tailwind-merge";
-import type { SavingsType } from "../../financeTypes";
+import type { MonthlySavings, SavingsType } from "../../financeTypes";
 import Container from "../Container";
 import SavingRecord from "./SavingRecord";
-import { groupSavingsByMonth } from "../../utils";
 
 export default function SavingsContainer({
-  current_savings,
+  monthly_savings,
 }: {
-  current_savings: SavingsType[];
+  monthly_savings: MonthlySavings;
 }) {
-  const monthly_savings = groupSavingsByMonth(current_savings);
+  const len = Object.keys(monthly_savings).length;
 
   return (
     <Container
       className={twMerge(
         "min-h-[30vh]",
-        current_savings.length === 0 && "grid place-items-center"
+        len === 0 && "grid place-items-center"
       )}
     >
-      {current_savings.length > 0 ? (
+      {len > 0 ? (
         <div className="overflow-hidden w-full rounded-lg shadow-inner border border-gray-100">
           {Object.keys(monthly_savings).map((month: string, id: number) => {
             return (
-              <div>
+              <div key={id}>
                 <p
                   className={twMerge(
-                    "w-full ml-2 my-2 font-bold max-w-[800px]",
-                    id === 0 ? "" : "mt-6"
+                    "w-full ml-2 font-bold max-w-[800px]",
+                    id === 0 ? "mt-2" : "mt-4"
                   )}
-                  key={id}
                 >
                   {month}
                 </p>
